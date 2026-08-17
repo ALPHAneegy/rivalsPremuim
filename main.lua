3048957ac24f792b5d355185f47931c46789eb2c
ALPHAneegy <andidiazas@gmail.com>
2026-08-18 00:13:08 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -262,7 +262,7 @@ getgenv().Method = nil
 _qot = queue_on_teleport
 
 function queue_on_teleport(p40)
-    p40:gsub('https://dev-8bitsforya.pantheonsite.io/nan.lua', 'https://raw.githubusercontent.com/ALPHAneegy/rivalsPremuim/refs/heads/main/main.lua')
+    p40:gsub('https://raw.githubusercontent.com/ALPHAneegy/rivalsPremuim/refs/heads/main/nan1.lua', 'https://raw.githubusercontent.com/ALPHAneegy/rivalsPremuim/refs/heads/main/main.lua')
 
     return _qot('getgenv().method = "2";getgenv().option = "premium";' .. p40)
 end
