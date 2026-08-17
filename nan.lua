5c0f1687f3881c8e852de78cfea6bfb2f373a346
ALPHAneegy <andidiazas@gmail.com>
2026-08-17 20:27:46 +0200

    Update nan.lua

--- nan.lua
+++ nan.lua
@@ -485,7 +485,7 @@ end
 
 local u69 = {
     Wlc = v62:AddTab({
-        Title = 'Vroy not porn',
+        Title = 'Boarding',
         Icon = 'rbxassetid://92947874955378',
     }),
     Main = v62:AddTab({
